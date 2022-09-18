d424b50e5522b6ed8b5ab8b23985fe29ddc04282
fishcka <l.afanasenko@gmail.com>
2022-09-18 14:59:26 +0300

    registration test

--- test_registration.py
+++ test_registration.py
@@ -3,7 +3,8 @@
 # або чатсково випадковими (Оскільки той самий юзер не може бути зареєстрований двічі)
 # Нюанс номер 2: Вам потрібно самостійно придумати перевірку, що буде підверджувати успішність реєстрації.
 # Це може бути перевірка наявності якогось поля, його значення, повідомлення або первірка URL.
-
+import csv
+import logging
 import random
 import string
 from time import sleep
@@ -12,6 +13,7 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+log = logging.getLogger(__name__)
 characters = list(string.ascii_letters + string.digits)
 
 
@@ -30,43 +32,119 @@ def random_symbols():
     return symbols
 
 
-@pytest.fixture(scope="function")
-def login_page():
+@pytest.fixture(scope="class")
+def page_driver():
     # Create driver
     driver = webdriver.Firefox()
-    # Maximize window
-    driver.maximize_window()
     # Open page
     driver.get("https://qa-complex-app-for-testing.herokuapp.com/")
+    # Maximize window
+    driver.maximize_window()
+    log.info("firefox window opened")
     yield driver
     # Close driver
     driver.close()
 
 
 class TestRegistration:
-    @pytest.mark.repeat(1)  # the value in brackets indicates how many times to repeat the test
-    def test_registration(self, random_symbols, login_page):
+    log = logging.getLogger(__name__)
+
+    with open("usernames.csv", "r") as f:
+        reader = csv.reader(f, delimiter=",")
+        usernames = [tuple(row) for row in reader]
+
+    @pytest.mark.repeat(1)
+    @pytest.mark.parametrize(["username", "user_error", "user_log"], usernames)
+    def test_registration_username_field(self, page_driver, username, user_error, user_log):
+        # Fill username
+        login = page_driver.find_element(by=By.XPATH, value=".//input[@id='username-register']")
+        login.send_keys(username)
+        self.log.info(f"'{username}' entered to username field")
+        sleep(2)
+
+        # Verify error
+        err_mess = page_driver.find_element(by=By.XPATH, value=".//div[@class='alert alert-danger small liveValidateMessage liveValidateMessage--visible']")
+        assert err_mess.text == user_error, f"Actual message: {err_mess.text}"
+        login.clear()
+        self.log.info(user_log)
+        sleep(2)
+
+    @pytest.mark.repeat(1)
+    def test_registration_email_field(self, page_driver):
+        # Fill email without @ symbol
+        email = page_driver.find_element(by=By.XPATH, value=".//input[@id='email-register']")
+        email.send_keys("randommail")
+        self.log.info("'randommail' entered to email field")
+        sleep(2)
+
+        # Verify error
+        err_mess = page_driver.find_element(by=By.XPATH, value=".//div[contains(text(),'You must provide a valid email address.')]")
+        assert err_mess.text == "You must provide a valid email address.", f"Actual message: {err_mess.text}"
+        email.clear()
+        self.log.info("email without @ verified")
+        sleep(2)
+
+    @pytest.mark.repeat(1)
+    def test_registration_password_field(self, page_driver):
+        # Fill password with less than 12 symbols
+        password = page_driver.find_element(by=By.XPATH, value=".//input[@id='password-register']")
+        password.send_keys("pass")
+        self.log.info("'pass' entered to password field")
+        sleep(2)
+
+        # Verify error
+        err_mess = page_driver.find_element(by=By.XPATH, value=".//div[contains(text(),'Password must be at least 12 characters.')]")
+        assert err_mess.text == "Password must be at least 12 characters.", f"Actual message: {err_mess.text}"
+        password.clear()
+        self.log.info("password with less than 12 symbols verified")
+        sleep(2)
+
+        # Fill password with more than 50 symbols
+        password.send_keys("veryverylongpasswordmuchmorethanfiftycharacterslong")
+        self.log.info("'veryverylongpasswordmuchmorethanfiftycharacterslong' entered to password field")
+        sleep(2)
+
+        # Verify error
+        err_mess = page_driver.find_element(by=By.XPATH, value=".// div[contains(text(), 'Password cannot exceed 50 characters.')]")
+        assert err_mess.text == "Password cannot exceed 50 characters.", f"Actual message: {err_mess.text}"
+        password.clear()
+        self.log.info("password with more than 50 symbols verified")
+        sleep(2)
+
+    @pytest.mark.repeat(1)
+    def test_registration(self, random_symbols, page_driver):
         # Fill login
-        login = login_page.find_element(by=By.XPATH, value=".//input[@id='username-register']")
+        login = page_driver.find_element(by=By.XPATH, value=".//input[@id='username-register']")
         login.send_keys(random_symbols)
-        sleep(1)
+        self.log.info(f"'{random_symbols}' entered to username field")
+        sleep(2)
 
         # Fill email
-        password = login_page.find_element(by=By.XPATH, value=".//input[@id='email-register']")
-        password.send_keys(random_symbols + "@gmail.com")
-        sleep(1)
+        email = page_driver.find_element(by=By.XPATH, value=".//input[@id='email-register']")
+        email.send_keys(random_symbols + "@gmail.com")
+        self.log.info(f"'{random_symbols}@gmail.com' entered to email field")
+        sleep(2)
 
         # Fill password
-        password = login_page.find_element(by=By.XPATH, value=".//input[@id='password-register']")
+        password = page_driver.find_element(by=By.XPATH, value=".//input[@id='password-register']")
         password.send_keys(random_symbols)
-        sleep(1)
+        self.log.info(f"'{random_symbols}' entered to password field")
+        sleep(2)
 
-        # Click button
-        button = login_page.find_element(by=By.XPATH, value=".//button[@type='submit']")
+        # Click 'Sign up for OurApp' button
+        button = page_driver.find_element(by=By.XPATH, value=".//button[@type='submit']")
         button.click()
-        sleep(1)
+        self.log.info(f"button 'Sign up for OurApp' clicked")
+        sleep(2)
 
         # Verify registration
-        profile_button = login_page.find_element(by=By.XPATH, value=".//img[@alt='My profile']")
+        profile_button = page_driver.find_element(by=By.XPATH, value=".//img[@alt='My profile']")
         assert profile_button.is_enabled(), f"Registration failed"
+        self.log.info("registration was successful")
+        sleep(2)
+
+        # Click 'Sign Out' button
+        button = page_driver.find_element(by=By.XPATH, value=".//button[@class='btn btn-sm btn-secondary']")
+        button.click()
+        self.log.info(f"button 'Sign Out' clicked")
         sleep(2)
